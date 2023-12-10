158da7934704ca04def8650fd682072b92920b47
Francesco Ivone <francesco.ivone@cern.ch>
2023-12-10 19:35:46 +0100

    ignore

---
+++ docs/index.md
@@ -0,0 +1,23 @@
+### Vectorization
+
+I am trying to understand what is the fastest way to handle operations on uint64 (bitboards)
+I am trying to use the **SIMD** (Single Instruction Multiple Data).
+I have started with the `immintrin.h` library. 
+So far my conclusion is that the compiler already implements this parallelization when possible.
+For example, when adding 2 vectors of int together the speed-up is noticeable:
+* *without* compiler optimization
+    ```bash
+    gcc  main.cpp -lstdc++ -o main; ./main
+    Took 1.97542
+    ```
+* *with* compiler optimization
+    ```bash
+    gcc -O3  -march=native -ftree-vectorize main.cpp -lstdc++ -o main; ./main
+    Took 0.140917
+    ```
+Which is a factor 10 speed up.
+Therefore, I have added these settings in the CMake
+```
+# Compiler flags
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3  -march=native -ftree-vectorize -lstdc++")
+```
--- src/main.cpp
+++ src/main.cpp
@@ -2,83 +2,11 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
-// #include <immintrin.h>
-
-using Vector=std::vector<int>;
-
-// Vector simAdd(const Vector& a, const Vector& b){
-//     Vector result(a.size());
-
-//     constexpr auto FLOATS_IN_AVX_REGISTER = 8u;
-//     const auto itemsWithSIMD = (a.size()/ FLOATS_IN_AVX_REGISTER) * FLOATS_IN_AVX_REGISTER;
-
-//     auto i = 0u;
-//     // vectorized addition
-//     for(; i<itemsWithSIMD; i+=FLOATS_IN_AVX_REGISTER){
-//         // load from memory to the specific SMID register
-//         // u stands for unalligned data
-//         // ps stands for single precision (float)
-//         auto aRegister = _mm256_loadu_ps(a.data()+1);
-//         auto bRegister = _mm256_loadu_ps(b.data()+1);
-
-//         auto sum = _mm256_add_ps(aRegister, bRegister);
-
-//         // store "sum" in the result vector
-//         // that's defined in the memory
-//         _mm256_storeu_ps(result.data() + i, sum);
-//     }
-
-//     return result;
-// }
-
-Vector scalarAdd(const Vector& a, const Vector& b){
-    Vector result(a.size());
-
-    for(int i =0; i<result.size(); i++)
-        result[i] = a[i] + b[i];
-
-    return result;    
-}
-
 
 int main(){
 
-    using namespace std;
-    Vector a(17,1U);
-    int N = 100000;
-
-    chrono::steady_clock::time_point begin,end;
-    begin = chrono::steady_clock::now();
-    // for(int j =0; j < N; j++)
-    //     auto result = simAdd(a,a);
-    end = chrono::steady_clock::now();
-    cout<<"SIMD Took "<<chrono::duration_cast<chrono::microseconds>(end - begin).count()/1000000.<<endl;
-
-
-    begin = chrono::steady_clock::now();
-    for(int j =0; j < N; j++)
-        auto result = scalarAdd(a,a);
-    end = chrono::steady_clock::now();
-    cout<<"scalarAdd Took "<<chrono::duration_cast<chrono::microseconds>(end - begin).count()/1000000.<<endl;
-
-    
     return 1;
 
 }
 
 
-
-
-// int main(int argc, char **argv) {
-
-//     std::chrono::steady_clock::time_point begin,end;
-//     begin = std::chrono::steady_clock::now();
-//     int t = 0;
-//     for(int j =0; j < 100000000; j++)
-//         t++;
-
-//     end = std::chrono::steady_clock::now();
-//     std::cout<<"Took "<<std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()/1000000.<<std::endl;
-    
-//     return 1;
-// };
