4899ef3996ec0ff12c6b6161935b093622f5549f
Francesco Ivone <francesco.ivone@cern.ch>
2023-12-11 09:51:32 +0100

    Doxygen

--- includes/print.hpp
+++ includes/print.hpp
@@ -1,4 +1,9 @@
+/** @file */
 #pragma once
 #include "types.hpp"
 # include "colors.hpp"
+/*! Prints the given bitboardwith a # in place of the set bits.
+    \param BitBoard 
+    \return void
+*/  
 void RepresentBitset(Bitboard);
