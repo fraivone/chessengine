8ae597bb833d2bf72f18e634523b2d1a2d2b77b0
Francesco Ivone <francesco.ivone@cern.ch>
2024-01-07 20:06:20 +0100

    Successful test PERFTH depth = 1 with 12 FENs

--- test/boostTester.cpp
+++ test/boostTester.cpp
@@ -220,20 +220,27 @@ BOOST_AUTO_TEST_SUITE(ChessEngine)
                                     {"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 20},
                                     {"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -", 48},
                                     {"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ", 14},
-                                    {"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ", 14},
                                     {"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 6},
                                     {"rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",44},
-                                    {"r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",46}
+                                    {"r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",46},
+                                    {"r6r/1b2k1bq/8/8/7B/8/8/R3K2R b KQ - 3 2",8},
+                                    {"8/8/8/2k5/2pP4/8/B7/4K3 b - d3 0 3",8},
+                                    {"r1bqkbnr/pppppppp/n7/8/8/P7/1PPPPPPP/RNBQKBNR w KQkq - 2 2",19},
+                                    {"r3k2r/p1pp1pb1/bn2Qnp1/2qPN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQkq - 3 2",5},
+                                    {"2kr3r/p1ppqpb1/bn2Qnp1/3PN3/1p2P3/2N5/PPPBBPPP/R3K2R b KQ - 3 2",44},
+                                    {"rnb2k1r/pp1Pbppp/2p5/q7/2B5/8/PPPQNnPP/RNB1K2R w KQ - 3 9", 39},
+                                    {"2r5/3pk3/8/2P5/8/2K5/8/8 w - - 5 4",9}
         };
-        MoveList thePseudoMoves;
+
+        MoveList theMoveList;
         for (auto const& x : PERFTH1){
-        thePseudoMoves.Clear();
+        theMoveList.Clear();
         auto f = x.first;
         auto result = x.second;
         init_position(f);
-        thePseudoMoves = generate_all(thePseudoMoves, Position::sideToMove);
-        if(result == 44) PrintMoveList(thePseudoMoves);
-        BOOST_CHECK_EQUAL(thePseudoMoves.size,result);
+        theMoveList = generate_legal(Position::sideToMove);
+        // if(result == 14) PrintMoveList(theMoveList);
+        BOOST_CHECK_EQUAL(theMoveList.size,result);
     }
 
     }
