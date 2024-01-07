26da24d084570f293687a25b168c8e2c9bd268ea
Francesco Ivone <francesco.ivone@cern.ch>
2024-01-07 22:08:53 +0100

    gen_move now uses Ray. 100% perft depth 1 correct

--- src/gen_move.cpp
+++ src/gen_move.cpp
@@ -422,7 +422,9 @@ MoveList generate_legal(Color Us, Bitboard checkers){
                 // ray "from" to "to" including "to" but excluding "from"
                 movingTrace = BetweenBB[from][to] | toBB;
                 // ray "from" to "pinner" including "to" but excluding "pinner"
-                allowedPath = BetweenBB[from][pinner] | make_bitboard(pinner);
+                allowedPath = Ray[from][pinner];
+                if(from)
+                    std::cout<<"Pinner "<<+pinner<<std::endl;
                 // not only pinned, this move even tries to move the pinned piece
                 // out of the allowed path. Pop the move
                 if( (movingTrace & allowedPath) != movingTrace){
