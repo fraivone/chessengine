4cee837163f8a8076953825b84ed491e263ff386
Francesco Ivone <francesco.ivone@cern.ch>
2024-01-12 08:28:49 +0100

    Get a move from string

--- includes/move_maker.hpp
+++ includes/move_maker.hpp
@@ -15,3 +15,8 @@ void UndoMove(Move mv);
 uint64_t StupidPerftCount(MoveList mvList, uint8_t depth,uint8_t MaxDepth, bool verbose = false, uint64_t count = 0);
 /// count legal moves only at depth, non cumulative. Mimicks stockfish
 uint64_t StupidPerftCount(std::string FEN, uint8_t MAxDepth, bool verbose = false);
+/// Given a string in stockfish style (4 or 5 char with <sq_from><sq_to><promotion_type optional>)
+/// generates a move. 
+/// If the move is legal, it returns it.
+/// else return NULL_MOVE
+Move parse_move(std::string inputstring);
